4efd053a97c751526b34c7a3c010bf79828c5432
Saim7787 <ujamil5@gmail.com>
2024-03-20 11:36:05 -0700

    repo is ready

--- Src/Admin/Screens/AdminProfile/Index.jsx
+++ Src/Admin/Screens/AdminProfile/Index.jsx
@@ -1,39 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { View, Text, Image,Dimensions } from 'react-native';
-import { TabView, TabBar } from 'react-native-tab-view';
 import { ThemeContext } from '../../../Theme/ThemeContext';
 import { lightTheme, darkTheme } from '../../../Theme/Color';
 import { styles } from './Styles';
-import AcademicHistory from './AcademicHistory';
 import StudentProfile from './StudentProfile';
 import { FONTSIZE } from '../../../Theme/FontSize';
 import { FONTFAMILY } from '../../../Theme/FontFamily';
 
 
-const renderScene = ({ route }) => {
-  switch (route.key) {
-    case 'Student Profile':
-      return <StudentProfile />;
-    case 'Academic History':
-      return <AcademicHistory />;
-    default:
-      return null;
-  }
-};
-
-const renderTabBar = (props) => {
-    const themeContext = useContext(ThemeContext);
-    const theme = themeContext?.isDarkTheme ? darkTheme : lightTheme;
-  
-    return (
-      <TabBar
-        {...props}
-        indicatorStyle={{ backgroundColor: 'white' }} // Customize the indicator color
-        style={{ backgroundColor:'#0961F5',elevation:2,marginBottom:15 }} // Customize the tab bar background color
-        labelStyle={{fontSize:FONTSIZE.size_16,fontFamily:FONTFAMILY.Jost_SemiBold,textAlign:'center',}} // Customize the tab label color
-      />
-    );
-  };
 
 const AdminProfile = () => {
   const themeContext = useContext(ThemeContext);
@@ -56,16 +30,7 @@ const AdminProfile = () => {
         </View>
         <Text style={[styles.profile_name, { color: theme.primaryText }]}>James S. Hernandez</Text>
         <Text style={[styles.profile_mail, { color: theme.PrimarylightText }]}>hernandex.redial@gmail.ac.in</Text>
-
-        {/* TabView */}
-        <TabView
-          navigationState={{ index, routes }}
-          renderScene={renderScene}
-          renderTabBar={renderTabBar}
-          onIndexChange={setIndex}
-          initialLayout={{ width: Dimensions.get('window').width }}
-        />
-    
+        <StudentProfile />
     </View>
   );
 };
--- Src/Screens/Dashboard/Home/Index.js
+++ Src/Screens/Dashboard/Home/Index.js
@@ -1,14 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import Geolocation from '@react-native-community/geolocation'; // Using react-native-community/geolocation for location
+import styles from './Style'; // Assuming you have a Style.js file for styles
 
 const Index = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        setLocation(position);
+      },
+      error => {
+        setErrorMsg('Permission to access location was denied');
+      },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
+
+  
+
+  // function toggleCameraType() {
+  //   setType(current => (current === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back));
+  // }
+
+  // async function takePicture() {
+  //   if (cameraRef.current) {
+  //     const options = { quality: 1, base64: true, fixOrientation: true, exif: true };
+  //     const photo = await cameraRef.current.takePictureAsync(options);
+  //     setCapturedImage(photo.uri);
+  //   }
+  // }
+
+  // if (permission === null) {
+  //   // Permission status is still loading
+  //   return <View />;
+  // }
+
+  // if (!permission) {
+  //   // Camera permissions are not granted yet
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+  //       <Button onPress={() => alert('Grant Permission')} title="Grant Permission" />
+  //     </View>
+  //   );
+  // }
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`;
+  }
+
   return (
-    <View>
-      <Text>Index</Text>
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+      {/* <View style={styles.cameraContainer}>
+        <Camera ref={cameraRef} style={styles.camera} type={type}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={takePicture}>
+              <Text style={styles.text}>Take Picture</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+
+      {capturedImage && (
+        <View style={styles.imageContainer}>
+          <Text style={styles.paragraph}>Captured Image:</Text>
+          <Image source={{ uri: capturedImage }} style={styles.image} />
+        </View>
+      )} */}
     </View>
-  )
-}
+  );
+};
 
-export default Index
-
-const styles = StyleSheet.create({})
+export default Index;
